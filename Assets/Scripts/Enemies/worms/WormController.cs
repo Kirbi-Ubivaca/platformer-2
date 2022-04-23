77238d00d7a1bc4a9a58469e1e6214c1eb511d85
arttimofee <artm.timofeev.08@mail.ru>
2022-04-23 17:55:53 +1000

    муха

--- Assets/Scripts/Enemies/worms/WormController.cs
+++ Assets/Scripts/Enemies/worms/WormController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class WormController : MonoBehaviour
 {
     float velocity = 1.5f;
+    bool moveLeft = true;
+    public Transform groundDetect;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,18 @@ public class WormController : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.left * velocity * Time.deltaTime);
-        RaycastHit2D groundinfo = 
-        Physics2D.Raycast(grounDetect.position, Vector2.down, 1f);
+        RaycastHit2D groundInfo = 
+        Physics2D.Raycast(groundDetect.position, Vector2.down, 1f);
+        if(!groundInfo.collider)
+        {
+            if (moveLeft) {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+                moveLeft = false;
+            }
+            else {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveLeft = true;
+            }
+        }
     }
 }
